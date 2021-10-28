8646537d1f3202921acea6caeee2b4608dbaaeb7
Louis-Philippe Turmel <turmlo@2020spaces.com>
2021-10-27 23:01:16 -0400

    Updated index.server.tsx

--- pages/index.server.tsx
+++ pages/index.server.tsx
@@ -3,18 +3,16 @@ import PlayerLoader from "../components/Player.client";
 import { playerList } from "../utils/player";
 import { Suspense } from "react";
 
-import styles from "../styles/Index.module.css";
-
 export default function Home() {
 	return (
-		<div>
+		<div className="bg-gray-900 w-full h-screen flex flex-col justify-center items-center space-y-8 p-4">
 			<Head />
 
-			<div className={styles.vstack}>
-				<p>Mythic+</p>
-				<p className=""> Team </p>
+			<div className="flex flex-row space-x-2 text-3xl font-bold">
+				<p className="text-white">Mythic+</p>
+				<p className="text-green-400"> Team </p>
 			</div>
-			<div className={styles.vstack}>
+			<div className="flex flex-col w-full space-y-4 items-center">
 				{playerList.map((player) => (
 					<Suspense
 						fallback={<p>Loading...</p>}

7e66e5b3f58843096277c6c9b414eff4cc85c5f3
Louis-Philippe Turmel <turmlo@2020spaces.com>
2021-10-27 23:01:45 -0400

    Updated PlayerItem.client.tsx

--- components/PlayerItem.client.tsx
+++ components/PlayerItem.client.tsx
@@ -1,24 +1,50 @@
 import { FunctionComponent } from "react";
-import Player from "../types/Player";
+import Player, { DungeonRole } from "../types/Player";
 import { RAIDER_IO_URL } from "../utils/Api";
 import { getPlayerScoreColor } from "../utils/player";
-import Link from "next/link";
+import RoleIcon from "./RoleIcon";
+
 export interface PlayerItemProps {
 	player: Player;
 }
 
 const PlayerItem: FunctionComponent<PlayerItemProps> = ({ player }) => {
 	const scoreColor = getPlayerScoreColor(player);
-	console.log(player);
+
 	return (
-		<div>
-			<img src={player.thumbnail_url} />
-			<p style={{ color: scoreColor }}>
+		<div className="relative cursor-pointer transition-colors duration-200 hover:bg-gray-600 flex flex-row bg-gray-700 w-full sm:w-[400px] rounded-md shadow-lg p-4 justify-start border-2 border-gray-600">
+			<p
+				style={{ color: scoreColor }}
+				className="absolute top-2 right-4 font-semibold"
+			>
 				{Math.round(player.mythic_plus_scores.all)}
 			</p>
-			<Link href={RAIDER_IO_URL + player.realm + "/" + player.name}>
-				{player.name}
-			</Link>
+			<div className="font-semibold absolute bottom-2 right-4 flex flex-row space-x-2">
+				<p className="text-gray-500">Weekly:</p>
+				<p className="text-white">
+					{player.mythic_plus_weekly_highest_level_runs.length}/10
+				</p>
+			</div>
+			<div className="absolute top-[-10px] left-[-10px]">
+				<RoleIcon
+					role={player.active_spec_role.toLowerCase() as DungeonRole}
+				/>
+			</div>
+			<div className="flex flex-row space-x-4 text-xl  items-center">
+				<img
+					src={player.thumbnail_url}
+					className="rounded-full w-16 h-16 ring-2 ring-gray-600"
+				/>
+				<a
+					href={RAIDER_IO_URL + player.realm + "/" + player.name}
+					target="_blank"
+					className=""
+				>
+					<p className="text-white transition-colors truncate duration-200 font-semibold hover:text-green-400">
+						{player.name}
+					</p>
+				</a>
+			</div>
 		</div>
 		// <Accordion allowMultiple w="300px" maxWidth="600px">
 		// 	<AccordionItem w="100%" border="none">

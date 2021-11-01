4f614ac6b15a4ad7694ecb882927f9c8c5f03266
lpturmel <louisphilippe.turmel@gmail.com>
2021-11-01 10:50:08 -0400

    Added dungeonItem component

---
+++ components/DungeonItem.tsx
@@ -0,0 +1,19 @@
+import { FunctionComponent } from "react";
+import { Dungeon } from "../types/Dungeon";
+import DungeonIcon from "./DungeonIcon";
+
+export interface DungeonItemProps {
+	dungeon: Dungeon;
+}
+
+const DungeonItem: FunctionComponent<DungeonItemProps> = ({ dungeon }) => {
+	return (
+		<div className="flex  flex-row space-x-4 text-white">
+			<DungeonIcon dungeon={dungeon.short_name} />
+			<p className="font-bold">{dungeon.short_name} </p>
+			<p>{dungeon.mythic_level}</p>
+		</div>
+	);
+};
+
+export default DungeonItem;

d297900cfe97e7675c229009f3f8e3c887762308
lpturmel <louisphilippe.turmel@gmail.com>
2021-11-01 15:49:46 -0400

    Removed comments

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,12 @@
 import { AppProps } from "next/app";
-//import Head from "../components/Head";
-import Head from "next/head";
+import Head from "../components/Head";
 
 import "../styles/index.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
-			<Head>
-				<title> Mythic+ Team </title>
-				<meta charSet="UTF-8" />
-				<meta
-					name="description"
-					content="Casual Mythic + team tracking"
-				/>
-				<meta
-					name="keywords"
-					content="World of Warcraft, Dungeons, Mythic Plus, Mythic+"
-				/>
-				<meta name="author" content="Louis-Philippe Turmel" />
-				<meta
-					name="viewport"
-					content="width=device-width, initial-scale=1.0"
-				/>
-			</Head>
-			{/* <Head /> */}
+			<Head />
 			<Component {...pageProps} />
 		</>
 	);

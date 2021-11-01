791ef43f39c45515bd7d8bb8357f5aae38767a44
lpturmel <louisphilippe.turmel@gmail.com>
2021-11-01 10:50:17 -0400

    Added DungeonPanel component

---
+++ components/DungeonPanel.client.tsx
@@ -0,0 +1,21 @@
+import { Dungeon } from "../types/Dungeon";
+import { FunctionComponent } from "react";
+import DungeonItem from "./DungeonItem";
+import { sortDungeons } from "../utils/Dungeons";
+
+export interface DungeonPanelProps {
+	dungeons: Dungeon[];
+}
+
+const DungeonPanel: FunctionComponent<DungeonPanelProps> = ({ dungeons }) => {
+	const sortedDungeons = sortDungeons(dungeons);
+	return (
+		<div className="flex flex-col space-y-4">
+			{sortedDungeons.map((dungeon) => (
+				<DungeonItem key={dungeon.zone_id} dungeon={dungeon} />
+			))}
+		</div>
+	);
+};
+
+export default DungeonPanel;

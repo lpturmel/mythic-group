b24de18a7c087fe51eefbf63c7475a67d90d8353
lpturmel <louisphilippe.turmel@gmail.com>
2021-11-01 10:49:59 -0400

    Used new map for matching convenant name + picture

--- components/DungeonIcon.tsx
+++ components/DungeonIcon.tsx
@@ -1,13 +1,21 @@
 import Image from "next/image";
+import { FunctionComponent } from "react";
+import { DungeonNames } from "../types/Dungeon";
+import { covenants } from "../utils/Covenant";
 
 export interface DungeonIconProps {
-	dungeon: "dos" | "top" | "soa" | "sd" | "plague" | "nw" | "mots" | "hoa";
+	dungeon: DungeonNames;
 }
 
-const DungeonIcon: React.FunctionComponent<DungeonIconProps> = ({
-	dungeon,
-}) => {
-	return <Image width="16px" height="16px" src={dungeon} />;
+const DungeonIcon: FunctionComponent<DungeonIconProps> = ({ dungeon }) => {
+	return (
+		<Image
+			width="16px"
+			height="16px"
+			src={covenants[dungeon].picture}
+			className="w-8 h-8"
+		/>
+	);
 };
 
 export default DungeonIcon;

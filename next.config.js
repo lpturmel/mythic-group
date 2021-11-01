43927f5f40f803de05a18ec3625039206b87fd87
lpturmel <louisphilippe.turmel@gmail.com>
2021-11-01 10:47:31 -0400

    Commented headers section

--- next.config.js
+++ next.config.js
@@ -5,18 +5,18 @@ module.exports = {
 		concurrentFeatures: true,
 		serverComponents: true,
 	},
-	async headers() {
-		return [
-			{
-				source: "/:path*",
-				headers: [
-					{
-						key: "Content-Security-Policy",
-						value: "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; img-src *; connect-src 'self' raider.io;",
-						//Content-Security-Policy: default-src 'self' *.raider.io *.worldofwarcraft.com
-					},
-				],
-			},
-		];
-	},
+	// async headers() {
+	// 	return [
+	// 		{
+	// 			source: "/:path*",
+	// 			headers: [
+	// 				{
+	// 					key: "Content-Security-Policy",
+	// 					value: "default-src 'self'; style-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; img-src *; connect-src 'self' raider.io;",
+	// 					//Content-Security-Policy: default-src 'self' *.raider.io *.worldofwarcraft.com
+	// 				},
+	// 			],
+	// 		},
+	// 	];
+	// },
 };

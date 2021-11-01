695e15bf70a75099eb01ab6dc9908d63a5866713
lpturmel <louisphilippe.turmel@gmail.com>
2021-11-01 16:32:43 -0400

    Updated dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import { AppProps } from "next/app";
+import Head from "../components/Head";
 import "../styles/index.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
-			{/* <Head /> */}
+			<Head />
 			<Component {...pageProps} />
 		</>
 	);

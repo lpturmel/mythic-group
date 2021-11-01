4bd6ab83dfd602f12a1ca81ea208c7bf612d6988
lpturmel <louisphilippe.turmel@gmail.com>
2021-11-01 15:35:11 -0400

    Updated _app.tsx to use meta tags directly

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,31 @@
 import { AppProps } from "next/app";
-import Head from "../components/Head";
+//import Head from "../components/Head";
+import Head from "next/head";
 
 import "../styles/index.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
-			{" "}
-			<Head /> <Component {...pageProps} />{" "}
+			<Head>
+				<title> Mythic+ Team </title>
+				<meta charSet="UTF-8" />
+				<meta
+					name="description"
+					content="Casual Mythic + team tracking"
+				/>
+				<meta
+					name="keywords"
+					content="World of Warcraft, Dungeons, Mythic Plus, Mythic+"
+				/>
+				<meta name="author" content="Louis-Philippe Turmel" />
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1.0"
+				/>
+			</Head>
+			{/* <Head /> */}
+			<Component {...pageProps} />
 		</>
 	);
 }

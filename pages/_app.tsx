d620897f6f7a380a56db54438b941cc59ff22941
lpturmel <louisphilippe.turmel@gmail.com>
2021-11-01 18:07:15 -0400

    Updated _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,27 @@
 import { AppProps } from "next/app";
-import Head from "../components/Head";
+import Head from "next/head";
 import "../styles/index.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<>
-			<Head />
+			<Head>
+				<title> Mythic+ Team </title>
+				<meta charSet="UTF-8" />
+				<meta
+					name="description"
+					content="Casual Mythic + team tracking"
+				/>
+				<meta
+					name="keywords"
+					content="World of Warcraft, Dungeons, Mythic Plus, Mythic+"
+				/>
+				<meta name="author" content="Louis-Philippe Turmel" />
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1.0"
+				/>
+			</Head>
 			<Component {...pageProps} />
 		</>
 	);
